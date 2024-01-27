661c4b9edb87a941ec57aaad546156f81da2e7a4
Pallavi <pm7986935149@gmail.com>
2024-01-27 09:59:53 +0530

    Igniting our app

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const heading = React.createElement("h1", {}, "hello world from react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
